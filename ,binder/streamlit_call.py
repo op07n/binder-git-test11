ecbf960b26c118702365552d8754ee84aa1c0da1
op07n <39182105+op07n@users.noreply.github.com>
2020-02-27 12:39:21 +0100

    Delete streamlit_call.py

--- ,binder/streamlit_call.py
+++
@@ -1,7 +0,0 @@
-from subprocess import Popen
-import os
-
-def load_jupyter_server_extension(nbapp):
-    """serve the streamlit app"""
-    os.chdir('tech-news-cluster/tech_news_cluster')
-    Popen(["streamlit", "run", "streamlit_app.py", "--browser.serverAddress=0.0.0.0", "--browser.gatherUsageStats=False", "--server.enableCORS=False"])
